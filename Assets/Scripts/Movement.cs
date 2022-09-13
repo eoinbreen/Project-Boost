9da37f2ec0b5cb0089b1a371b45238c195caa6bb
eoinbreen <e.breen185@gmail.com>
2022-09-13 20:27:42 +0100

    Added Audio

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rocketRigidBody;
+    [SerializeField] float rocketThrust = 1f;
+    [SerializeField] float rotationSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    //processes input to add thrust to the Rocket
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketRigidBody.AddRelativeForce(Vector3.up * rocketThrust * Time.deltaTime);
+        }
+    }
+
+    //Processes input to rotate the rocket
+    void ProcessRotation()
+    { 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))//dont rotate both ways at a time
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+    
+    //applies rotation to the rocket
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rocketRigidBody.freezeRotation = true; //Freeze rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rocketRigidBody.freezeRotation = false; //Unfreeze rotation
+    }
+}
